caed4c3431d90bac1474588c574cc1ff409ae497
leonelcolina <tang.leonel@gmail.com>
2022-08-22 00:34:21 -0400

    finished splash page

--- frontend/components/splash/infinite_posts.jsx
+++ frontend/components/splash/infinite_posts.jsx
@@ -7,6 +7,7 @@ const InfinitePosts = ({ currentUser }) => {
             title: "How not to kill your cohort",
             body: "lorem ipsum whatever I don't remember",
             date: "Aug 19",
+            tag: "self-control",
             length: "125 min read"
         },
         6: {
@@ -14,6 +15,7 @@ const InfinitePosts = ({ currentUser }) => {
             title: "How to encourage students that suck",
             body: "lorem ipsum whatever I don't remember",
             date: "Aug 19",
+            tag: "happiness",
             length: "35 min read"
         },
         1: {
@@ -21,6 +23,7 @@ const InfinitePosts = ({ currentUser }) => {
             title: "Agile Projects Have Become Waterfall Projects With Sprints",
             body: "lorem ipsum whatever I don't remember",
             date: "Aug 17",
+            tag: "water",
             length: "4 min read"
         },
         2: {
@@ -28,6 +31,7 @@ const InfinitePosts = ({ currentUser }) => {
             title: "Radiation is cool",
             body: "lorem ipsum whatever I don't remember",
             date: "Jul 17",
+            tag: "random",
             length: "64 min read"
         },
         3: {
@@ -35,19 +39,20 @@ const InfinitePosts = ({ currentUser }) => {
             title: "Supes ain't it",
             body: "lorem ipsum whatever I don't remember",
             date: "Sep 4",
+            tag: "superhero",
             length: "5 min read"
         },
         4: {
             author: "Vivian Polonski",
             title: "I bought a plant",
             body: "lorem ipsum whatever I don't remember",
+            tag: "loneliness",
             date: "May 7",
             length: "4 min read"
         }
     }
 
     const infinitePosts = () => (
-        <>
         <div className="outer-infinite-posts-container">
             <div className="inner-infinite-posts-container">
                 <div className="infinite-posts-grid-container">
@@ -88,7 +93,10 @@ const InfinitePosts = ({ currentUser }) => {
                                                                 <span>{post.length}</span>
                                                             </div>
                                                             <div className="infinite-post-save">
-                                                                
+                                                                <svg width="25" height="25" viewBox="0 0 25 25" fill="none" class="mg">
+                                                                    <path d="M18 2.5a.5.5 0 0 1 1 0V5h2.5a.5.5 0 0 1 0 1H19v2.5a.5.5 0 1 1-1 0V6h-2.5a.5.5 0 0 1 0-1H18V2.5zM7 7a1 1 0 0 1 1-1h3.5a.5.5 0 0 0 0-1H8a2 2 0 0 0-2 2v14a.5.5 0 0 0 .8.4l5.7-4.4 5.7 4.4a.5.5 0 0 0 .8-.4v-8.5a.5.5 0 0 0-1 0v7.48l-5.2-4a.5.5 0 0 0-.6 0l-5.2 4V7z" fill="#292929">
+                                                                    </path>
+                                                                </svg>
                                                             </div>
                                                         </div>
 
@@ -104,13 +112,76 @@ const InfinitePosts = ({ currentUser }) => {
                             {/* ------------------------ */}
                         </section>
                         <aside className="tags-footer-container">
-
+                            <div className="side-container">
+                                <div className="discover-what-matters-container">
+                                    <div className="discover-title">
+                                        <p>discover more of what matters to you</p>
+                                    </div>
+                                    <div className="discover-interests">
+                                            {Object.values(POSTMOCK).map((post, idx) =>{
+                                                return (
+                                                    <a href="#" key={idx} className="side-interest">
+                                                        <div>
+                                                            {post.tag}
+                                                        </div>
+                                                    </a>
+                                                )
+                                            })}
+                                    </div>
+                                </div>
+                                <div className="side-footer-links">
+                                        <div className="footer-link">
+                                            <a href="#">
+                                                Help
+                                            </a>
+                                        </div>
+                                        <div className="footer-link">
+                                            <a href="#">
+                                                Status
+                                            </a>
+                                        </div>
+                                        <div className="footer-link">
+                                            <a href="#">
+                                                Writers
+                                            </a>
+                                        </div>
+                                        <div className="footer-link">
+                                            <a href="#">
+                                                Blog
+                                            </a>
+                                        </div>
+                                        <div className="footer-link">
+                                            <a href="#">
+                                                Careers
+                                            </a>
+                                        </div>
+                                        <div className="footer-link">
+                                            <a href="#">
+                                                Privacy
+                                            </a>
+                                        </div>
+                                        <div className="footer-link">
+                                            <a href="#">
+                                                Terms
+                                            </a>
+                                        </div>
+                                        <div className="footer-link">
+                                            <a href="#">
+                                                About
+                                            </a>
+                                        </div>
+                                        <div className="footer-link">
+                                            <a href="#">
+                                                Knowable
+                                            </a>
+                                        </div>
+                                </div>
+                            </div>
                         </aside>
                     </div>
                 </div>
             </div>
         </div>
-        </>
     );
 
     const emptyDiv = () => (
--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -7,7 +7,7 @@ const SplashPage = ({ currentUser, openModal }) => {
             <div className="splash-yellow-container">
                 <div id="stay-curious-div">
                     <div id="stay-curious-flex-div">
-                        <h1>Stay Curious.</h1>
+                        <h1>Stay curious.</h1>
                         <p>Discover stories, thinking, and</p>
                         <p>expertise from writers on any topic.</p>
                         <button onClick={() => openModal('signup')} id="start-reading-button">Start reading</button>
