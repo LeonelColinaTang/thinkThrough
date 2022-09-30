601046335b934c3a8107b561709bf4b17af1ba86
leonelcolina <tang.leonel@gmail.com>
2022-09-29 23:47:30 -0400

    Fix conditional logout button

--- frontend/components/splash_page/logged_in_nav.jsx
+++ frontend/components/splash_page/logged_in_nav.jsx
@@ -7,12 +7,10 @@ import { logoutUser } from "../../actions/session_actions";
 const LoggedInNav = () =>{
 
     const dispatch = useDispatch(); 
-    const currentUser = useSelector((state)=>{
-        
-    })
+    const currentUser = Boolean(useSelector((state)=> state.session.id))
+    console.log("CURRENT",currentUser)
     
     return(
-
         <nav className="logged-in-nav">
             <div className="buttons-container">
                 <div>
@@ -37,7 +35,7 @@ const LoggedInNav = () =>{
                     <div className="left-navbar-icons-container">
                         <a href="#">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" aria-label="Stories"><path d="M4.75 21.5h14.5c.14 0 .25-.11.25-.25V2.75a.25.25 0 0 0-.25-.25H4.75a.25.25 0 0 0-.25.25v18.5c0 .14.11.25.25.25z" stroke="currentColor"></path><path d="M8 8.5h8M8 15.5h5M8 12h8" stroke="currentColor" strokeLinecap="round"></path></svg>
-                        </a>
+                        </a>    
                     </div>
                     <div className="left-navbar-white-space">
                         <hr />
@@ -51,7 +49,7 @@ const LoggedInNav = () =>{
                 <div className="navbar-profile-pic">
                     <a href="#" className="navbar-profile-link">
                         <img src={window.smallGithubLogo} />
-                        <button className="header-button" onClick={()=>dispatch(logoutUser())}>Log Out</button>
+                        {currentUser && <button className="header-button" onClick={()=>dispatch(logoutUser())} value="Log Out">Log Out</button>}
                     </a>
                 </div>
             </div>
