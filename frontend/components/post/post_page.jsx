37ffb7a824f40e94f090bc2366bdb917c6e5f9be
leonelcolina <tang.leonel@gmail.com>
2022-09-29 22:15:10 -0400

    Add navbar

--- frontend/components/post/post_page.jsx
+++ frontend/components/post/post_page.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import moment from 'moment';
+import Post from "./post";
+import LoggedInNav from "../splash_page/logged_in_nav";
 
 
 class PostPage extends React.Component {
@@ -34,35 +36,12 @@ class PostPage extends React.Component {
         const {post} = this.state;
         console.log(post)
         return(
-            <div className="post-page-div">
-                <div className="author-info-header">
-                    <div className="author-avatar">
-                        <img src={window.kyle} />
-                    </div>
-                    <div className="author-info">
-                        <div className="author-name">
-                            {/* <h3>{entirePost.author}</h3> */}
-                            <h3>Kyle Ginzburg</h3>
-                        </div>
-                        <div className="post-div-time">
-                            <p>{moment(post.created_at).format('MMMM D')}</p>
-                            <p>.</p>
-                            <p>{post.length}</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="entire-post-title">
-                    <h1>{post.title}</h1>
-                </div>
-                <div className="post-big-image">
-                    {/* here goes the image */}
-                    <img src={window.postPicture} />
-                </div>
-                <div className="entire-post-body">
-                    <p>{post.body}</p>
-                        <p>lorem ipsum whatever I don't remember Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat beatae velit sunt a recusandae similique debitis ratione excepturi? Qui quae laudantium, deserunt inventore nostrum commodi, pariatur ut voluptas nam accusamus consequuntur dignissimos ipsum culpa voluptatum ratione ex praesentium hic odit rem deleniti dolores veniam! Ea maiores porro eos, at sequi labore consectetur, maxime non minima impedit assumenda fuga. Id, nostrum a?</p>
-                </div>
+            <div>
+                <LoggedInNav />
+                <Post post={post} />
             </div>
+            
+
         )
     }
 
