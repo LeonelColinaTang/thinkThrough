60d7ea332b8ffceba919262b3d3efb244c17f880
leonelcolina <tang.leonel@gmail.com>
2022-10-04 23:15:56 -0400

    Delete unused import

--- frontend/components/post/post_page.jsx
+++ frontend/components/post/post_page.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Post from "./post";
 import LoggedInNav from "../splash_page/logged_in_nav";
-// import AsideMainSection from "../splash_page/aside_main_section";
 import AsidePost from "./aside_post";
 
 class PostPage extends React.Component {
