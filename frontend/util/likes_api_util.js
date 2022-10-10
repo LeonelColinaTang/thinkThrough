c8c40f7cf34ca336bc713fc379457b9f7533433b
leonelcolina <tang.leonel@gmail.com>
2022-10-09 23:59:13 -0400

    Add delete request

--- frontend/util/likes_api_util.js
+++ frontend/util/likes_api_util.js
@@ -2,6 +2,14 @@ export const createLike = (like) =>(
     $.ajax({
         url: 'api/likes',
         method: 'POST',
-        like
+        data: {like}
+    })
+)
+
+export const deleteLike = (id) => (
+    $.ajax({
+        url:`api/likes/${id}`,
+        method: `DELETE`,
+        data: {id}
     })
 )
