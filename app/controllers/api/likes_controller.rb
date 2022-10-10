4dc38819a0d4640df7d7458eda00f6c4e8613b5e
leonelcolina <tang.leonel@gmail.com>
2022-10-09 23:55:00 -0400

    Create likes controller

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,2 +1,28 @@
 class LikesController < ApplicationController
+
+    def create
+        @like = Like.new(like_params)
+        if !@like.save
+            render json: @comment.errors.full_messages, status: 422
+        end
+
+        redirect_to @like.post
+    end
+
+    def destroy
+        @like = Like.find(params[:id])
+        if @like.destroy
+            render json: params[:id]
+        else
+            render json: @like.errors.full_messages, status: 422
+        end
+    end
+
+
+    private
+    def like_params
+        params.require(:like).permit(:user_id, :post_id)
+    end
+    
+    
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-    validates :user_id, :post_id, presence: true 
+    validates :user_id, uniqueness: {scope: :post_id}
 
     belongs_to :user,
         primary_key: :id,
