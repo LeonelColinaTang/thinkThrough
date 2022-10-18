e58a97359416870adbc714f8c57a31f836fb1e31
leonelcolina <tang.leonel@gmail.com>
2022-10-17 23:31:47 -0400

    Update github link

--- frontend/components/landing_page/landing_navbar.jsx
+++ frontend/components/landing_page/landing_navbar.jsx
@@ -11,7 +11,7 @@ const LandingNavbar = () =>{
             <a href="#" className="think-through small-logo"><img src={window.smallMediumLogo} /><span>Thinkthrough</span></a>
             <div className="login-signup">
                 <a href="https://www.linkedin.com/in/leonel-colina-455531118/" target="_blank"><img src={window.smallLinkedinLogo} className="small-logo"/></a>
-                <a href="https://github.com/leonel040792" target="_blank"><img src={window.smallGithubLogo} className="small-logo" /></a>
+                <a href="https://github.com/LeonelColinaTang" target="_blank"><img src={window.smallGithubLogo} className="small-logo" /></a>
                 <a href="https://angel.co/u/leonel-colina" target="_blank"><img src={window.smallAngelistLogo} className="small-logo" /></a>
                 <a href="#" onClick={() => dispatch(openModal('login'))} id="login-navbar-link">Sign in</a>
                 <button onClick={() => dispatch(openModal('signup'))} className="get-started-button">Get Started</button>
